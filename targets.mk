97f74a757449b310b2ffaaf40cb6137237912975
Juan Cruz Viotti <jv@jviotti.com>
2021-03-18 10:52:16 -0400

    Add small TODO
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- targets.mk
+++ targets.mk
@@ -5,5 +5,7 @@ TYPESCRIPT_CONFIG_DIRECTORIES ?= lib test
 eslint:
 	./node_modules/.bin/eslint $(TYPESCRIPT_CONFIG_DIRECTORIES)
 
+# TODO: Automatically copy *.d.ts files from input to output here
+# See https://github.com/Microsoft/TypeScript/issues/5112
 tsc:
 	./node_modules/.bin/tsc
