5a5edd6cd0baca9f7a625dc9e3a685f020c77a1b
Juan Cruz Viotti <jv@jviotti.com>
2021-05-24 15:26:17 -0400

    Fix targets location
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- targets.mk
+++ targets.mk
@@ -3,9 +3,9 @@
 TYPESCRIPT_CONFIG_DIRECTORIES ?= lib test
 
 eslint:
-	./node_modules/.bin/eslint $(TYPESCRIPT_CONFIG_DIRECTORIES)
+	./vendor/typescript-config/node_modules/.bin/eslint $(TYPESCRIPT_CONFIG_DIRECTORIES)
 
 # TODO: Automatically copy *.d.ts files from input to output here
 # See https://github.com/Microsoft/TypeScript/issues/5112
 tsc:
-	./node_modules/.bin/tsc
+	./vendor/typescript-config/node_modules/.bin/tsc
