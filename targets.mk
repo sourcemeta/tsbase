f190663f3339a883c7da5e1c61c3d7ddca964e93
Juan Cruz Viotti <jv@jviotti.com>
2021-05-24 19:30:48 -0400

    Revert "Fix targets location"
    This reverts commit 5a5edd6cd0baca9f7a625dc9e3a685f020c77a1b.

--- targets.mk
+++ targets.mk
@@ -3,9 +3,9 @@
 TYPESCRIPT_CONFIG_DIRECTORIES ?= lib test
 
 eslint:
-	./vendor/typescript-config/node_modules/.bin/eslint $(TYPESCRIPT_CONFIG_DIRECTORIES)
+	./node_modules/.bin/eslint $(TYPESCRIPT_CONFIG_DIRECTORIES)
 
 # TODO: Automatically copy *.d.ts files from input to output here
 # See https://github.com/Microsoft/TypeScript/issues/5112
 tsc:
-	./vendor/typescript-config/node_modules/.bin/tsc
+	./node_modules/.bin/tsc
